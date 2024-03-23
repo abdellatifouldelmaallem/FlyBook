82ddf06fefab892a091cea5f3a4992c4048b6c97
unknown <ouldeabdellatif40@gmail.com>
2024-03-23 15:03:14 +0000

    declaration of components which added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './back_office/sidenav/sidenav.component';
+import { HeaderComponent } from './back_office/header/header.component';
+import { ProfileComponent } from './back_office/profile/profile.component';
+import { UsersComponent } from './back_office/users/users.component';
+import { HomeComponent } from './back_office/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HeaderComponent,
+    ProfileComponent,
+    UsersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
