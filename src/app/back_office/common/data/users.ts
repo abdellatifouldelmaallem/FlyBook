ee6f6432e6d9c0b87d85dcb8481c403552ddc608
unknown <ouldeabdellatif40@gmail.com>
2024-03-26 00:05:58 +0000

    RF5: implement the interface and provide some static data

---
+++ src/app/back_office/common/data/users.ts
@@ -0,0 +1,32 @@
+import { User } from '../interface/UserProp';
+
+export const Users: User[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@example.com',
+    ticket: true,
+    role: 'Admin'
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    email: 'jane@example.com',
+    ticket: false,
+    role: 'Passenger'
+  },
+  {
+    id: 3,
+    name: 'Alice Johnson',
+    email: 'alice@example.com',
+    ticket: true,
+    role: 'Passenger'
+  },
+  {
+    id: 4,
+    name: 'Bob Williams',
+    email: 'bob@example.com',
+    ticket: true,
+    role: 'Admin'
+  }
+];
---
+++ src/app/back_office/common/interface/UserProp.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id:number
+    name: string;
+    email: string;
+    ticket: boolean;
+    role: string;
+  }
+  
