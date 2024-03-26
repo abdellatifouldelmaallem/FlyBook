35ef23d1c8574843905b3e612c308896e4a2b049
unknown <ouldeabdellatif40@gmail.com>
2024-03-26 15:08:58 +0000

    RF2:confirmation delete

--- src/app/back_office/ticket/ticket.component.ts
+++ src/app/back_office/ticket/ticket.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Ticket } from '../common/interface/ticketProp';
 import { Tickets } from '../common/data/tickets';
+import { ConfirmationDeleteComponent } from '../shared/confirmation.delete/confirmation.delete.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-ticket',
@@ -9,6 +11,21 @@ import { Tickets } from '../common/data/tickets';
 })
 export class TicketComponent {
 
+  constructor(public dialog: MatDialog) {}
+
   Tickets: Ticket[] = Tickets;
+
+  dalete(){
+    const dialogRef = this.dialog.open(ConfirmationDeleteComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Item deleted');
+      } else {
+        console.log('Deletion canceled');
+      }
+    });
+
+  }
 
 }
--- src/app/back_office/users/users.component.ts
+++ src/app/back_office/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { User } from '../common/interface/userProp';
+import { User } from '../common/interface/UserProp';
 import { Users } from '../common/data/users';
+import { ConfirmationDeleteComponent } from '../shared/confirmation.delete/confirmation.delete.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-users',
@@ -9,5 +11,21 @@ import { Users } from '../common/data/users';
 })
 export class UsersComponent {
 
+  constructor(public dialog: MatDialog) {}
+
   Users: User[] = Users;
+
+  dalete(){
+    const dialogRef = this.dialog.open(ConfirmationDeleteComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Item deleted');
+      } else {
+        console.log('Deletion canceled');
+      }
+    });
+
+  }
+
 }
