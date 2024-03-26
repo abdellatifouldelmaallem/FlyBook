2a41475f0cd8d0f109f0812b1d27c35c30d02551
unknown <ouldeabdellatif40@gmail.com>
2024-03-26 14:59:40 +0000

    RF2: update the name of const

---
+++ src/app/back_office/common/data/flights.ts
@@ -0,0 +1,35 @@
+import { Flight } from "../interface/flightProp";
+
+export const Flights: Flight[] = [
+    {
+      flightName: "Flight 1",
+      date: "2024-04-01",
+      from: "New York",
+      to: "Los Angeles",
+      airport: "JFK",
+      airline: "Delta Airlines",
+      price: 350,
+      seat: "A01"
+    },
+    {
+      flightName: "Flight 2",
+      date: "2024-04-02",
+      from: "Los Angeles",
+      to: "Chicago",
+      airport: "LAX",
+      airline: "American Airlines",
+      price: 250,
+      seat: "B02"
+    },
+    {
+      flightName: "Flight 3",
+      date: "2024-04-03",
+      from: "Chicago",
+      to: "Dallas",
+      airport: "ORD",
+      airline: "United Airlines",
+      price: 200,
+      seat: "C03"
+    }
+  ]
+  
