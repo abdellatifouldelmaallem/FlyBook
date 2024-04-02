b3a41143d6ff89f099a729621c7515e5d4cbac9a
unknown <ouldeabdellatif40@gmail.com>
2024-04-02 17:31:09 +0000

    Added error handling for email and password fields and chenge the bg-color of sign-in page and sign-up page

--- src/app/front_office/sign-in/sign-in.component.ts
+++ src/app/front_office/sign-in/sign-in.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+//this.router.navigate(['/status']);
+signInForm: FormGroup = new FormGroup({});
+submitted = false;
+
+constructor(private formBuilder: FormBuilder,
+            private router:Router) { }
+
+ngOnInit(){
+  this.signInForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4)]],
+  })
+}
+
+get f() { return this.signInForm.controls; }
+  
+  
+onSubmit(){
+  this.submitted = true;
+
+  if (this.signInForm.invalid) {
+      return;
+  }
+  this.router.navigate(['/status']);
+  
+}
 
 }
