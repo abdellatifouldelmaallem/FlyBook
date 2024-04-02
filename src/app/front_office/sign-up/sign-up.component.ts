7f61f774f241e53b3d271c275d181bb67d9d3b25
unknown <ouldeabdellatif40@gmail.com>
2024-04-02 15:06:53 +0000

    add reactive form for sign-up form

--- src/app/front_office/sign-up/sign-up.component.ts
+++ src/app/front_office/sign-up/sign-up.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../_helper/must-match-validator';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+
+  signUpForm: FormGroup = new FormGroup({});
+  submitted = false;
+  
+  constructor(private formBuilder: FormBuilder) { }
+  
+  ngOnInit(){
+    this.signUpForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmPassword: ['', Validators.required],
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  get f() { return this.signUpForm.controls; }
+  
+  
+  onSubmit(){
+    this.submitted = true;
+
+    if (this.signUpForm.invalid) {
+        return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.signUpForm.value, null, 4));
+  }
 
 }
+
+
